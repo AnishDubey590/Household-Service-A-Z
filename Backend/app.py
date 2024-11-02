23e6d92699ed40fa91a4d69b95e4b2e5762ad13f
AnishDubey590 <23f2001087@ds.study.iitm.ac.in>
2024-11-02 20:00:14 +0530

    modified:   Backend/app.py new file:   instance/A-Z_household_services.db deleted:    Backend/__pycache__/app.cpython-312.pyc deleted:    Backend/app.py modified:   Backend/templates/Admin/home.html modified:   Backend/templates/Admin/service.html modified:   Backend/templates/customer_Signup.html modified:   Backend/templates/login.html deleted:    instance/A-Z_household_services.db

--- Backend/app.py
+++ Backend/app.py
@@ -1,30 +1,47 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+#Importing necessary libraries in app.py to create database and templates
+from flask import Flask, render_template,request # type: ignore
+from flask_sqlalchemy import SQLAlchemy # type: ignore
+from datetime import datetime
+import sys
 
+sys.path.insert(4,'E:\clone\Household-Service-A-Z-\Backend/Class')
+
+from Class import authentication_id,customer_add
+
+
+#Flask is class in python used to setup an web application
 app = Flask(__name__) 
-app.config['SQLALCHEMY_DATABASE_URI']="sqlite://A-Z_household_services.db"
-app.config['SQLALCHEMY_TRACK_MODIDFICATIONS']=False
-db = SQLAlchemy()
-class Customer(db.Model):
-    customer_id=db.Column(db.String(50) ,primary_key=True)
-    password=db.Column(db.String(50), nullable=False)
-    full_name=db.Column(db.String(50),nullable=False)
-    address=db.Column(db.String(200),nullable=False)
-    pin=db.Column(db.Integer(),nullable=False)
-    
+
+#Creating DataBase of name A-Z_household_services
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///AZ_household_services.db'
 
 
+# Setting up tracking changes to off 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+# creating an instance of the an app to store database
+db = SQLAlchemy(app)
+
+#Main login page
 @app.route("/login")
 def login():
     return render_template("login.html")
 
-@app.route("/sign_user")
+
+#Customer Signup page
+@app.route("/sign_user",methods=["GET","POST"])
 def signup_user():
+       
     return render_template("customer_Signup.html")
 
+
+#Professional Login page
 @app.route("/sign_professional")
 def signup_professor():
     return render_template("Professional_Signup.html")
+ 
 
+# Calling app to run
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
